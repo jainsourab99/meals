96fe5ef1529f2997aa7c87ce517875ad3260cbdf
Sourabh Jain <42956699+jainsourab99@users.noreply.github.com>
2023-09-21 10:13:52 +0530

    Explicit ANimation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,13 +5,38 @@ import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeal});
 
   final List<Meal> availableMeal;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration:const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, CategoryModel category) {
-    final filteredMeals = availableMeal
+    final filteredMeals = widget.availableMeal
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -28,7 +53,8 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return AnimatedBuilder(animation: _animationController,
+     child: GridView(
       padding: const EdgeInsets.all(16),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -45,6 +71,16 @@ class CategoriesScreen extends StatelessWidget {
             },
           )
       ],
-    );
+    ),
+    builder: (context, child) => SlideTransition(
+      position: _animationController.drive(
+        Tween(
+          begin: const Offset(1, 0.3),
+          end: const Offset(0, 0),
+          ),
+        ),
+                child: child,
+        ), 
+        );
   }
 }
